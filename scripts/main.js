615dfa5d9f71dcb18fc24c38838f567894ae2edf
DKrawczyk <d.krawczyk.nam@gmail.com>
2024-08-31 15:04:42 +0200

    save custo components

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("test")
-});
+    // const noteList = [{
+    //     title: "Note Title",
+    //     content: "Very long Note Body to give you an example on how the box should act",
+    //     date: "May 22"
+    // }]
+    const noteList = []
+    const addNoteBtn = document.getElementById('add-note')
+    addNoteBtn.addEventListener('click', () => {
+        console.log('test')
+    })
+
+    console.log(addNoteBtn)
+})
