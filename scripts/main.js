bc13e32169203653cd2283a12e9a848762cccbe9
DKrawczyk <d.krawczyk.nam@gmail.com>
2024-09-02 13:39:39 +0200

    Simple refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,109 +1,128 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const addNoteBtn = document.getElementById('add-note')
-    const cancelBtn = document.querySelector('.cancel-click')
-    const noNotesSection = document.querySelector('.no-content')
-    const newNoteSection = document.querySelector('.adding-note')
-    const noteTitle = document.getElementById('note-title')
-    const noteContent = document.getElementById('note-content')
-    const addButton = document.getElementById('insert-note')
-    const errorMessage = document.getElementById('error-message')
-    const noteForm = document.getElementById('note-form')
-    const addAnotherNoteBtn = document.getElementById('add-another-note')
-    const searchInput = document.getElementById('search')
-    const notesContainer = document.querySelector('.notes-wrapper')
-    const notes = document.querySelector('.notes')
+    const elements = {
+        addNoteBtn: document.getElementById('add-note'),
+        cancelBtn: document.querySelector('.cancel-click'),
+        noNotesSection: document.querySelector('.no-content'),
+        newNoteSection: document.querySelector('.adding-note'),
+        noteTitle: document.getElementById('note-title'),
+        noteContent: document.getElementById('note-content'),
+        addButton: document.getElementById('insert-note'),
+        errorMessage: document.getElementById('error-message'),
+        noteForm: document.getElementById('note-form'),
+        addAnotherNoteBtn: document.getElementById('add-another-note'),
+        searchInput: document.getElementById('search'),
+        notesContainer: document.querySelector('.notes-wrapper'),
+        notes: document.querySelector('.notes'),
+    }
 
-    addNoteBtn.addEventListener('click', () => {
-        noNotesSection.classList.toggle('hidden')
-        newNoteSection.classList.toggle('hidden')
-    })
+    const handleAddNoteClick = () => {
+        elements.noNotesSection.classList.toggle('hidden')
+        elements.newNoteSection.classList.toggle('hidden')
+    }
 
-    addAnotherNoteBtn.addEventListener('click', () => {
-        console.log(notes)
-        notes.style.maxHeight = '340px'
-        newNoteSection.classList.toggle('hidden')
-        addAnotherNoteBtn.classList.add('hidden')
-    })
+    const handleAddAnotherNoteClick = () => {
+        elements.notes.style.maxHeight = '340px'
+        elements.newNoteSection.classList.toggle('hidden')
+        elements.addAnotherNoteBtn.classList.add('hidden')
+    }
 
-    cancelBtn.addEventListener('click', () => {
+    const handleCancelClick = () => {
         const allNotes = document.querySelectorAll('.single-note')
-        newNoteSection.classList.add('hidden')
+        elements.newNoteSection.classList.add('hidden')
 
         if (allNotes.length === 0) {
-            noNotesSection.classList.remove('hidden')
+            elements.noNotesSection.classList.remove('hidden')
         } else {
-            addAnotherNoteBtn.classList.remove('hidden')
+            elements.addAnotherNoteBtn.classList.remove('hidden')
         }
-        notes.style.maxHeight = '650px'
-        noteForm.reset()
-    })
+        elements.notes.style.maxHeight = '650px'
+        elements.noteForm.reset()
+    }
+
+    const handleNoteInputChange = () => {
+        if (!validateForm()) {
+            elements.addButton.classList.add('hidden')
+        } else {
+            elements.addButton.classList.remove('hidden')
+        }
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+
+        if (validateForm()) {
+            const formData = {
+                title: elements.noteTitle.value.trim(),
+                content: elements.noteContent.value.trim(),
+                date: new Date().toLocaleDateString('pl-PL', { month: 'long', day: 'numeric' }),
+            }
+
+            addNewNote(formData)
+
+            elements.addButton.classList.add('hidden')
+            elements.newNoteSection.classList.toggle('hidden')
+            elements.addAnotherNoteBtn.classList.remove('hidden')
+            elements.notes.style.maxHeight = '650px'
+            elements.noteForm.reset()
+        }
+    }
+
+    const handleSearchInput = () => {
+        const allNotes = document.querySelectorAll('.single-note')
+        const searchTerm = elements.searchInput.value.toLowerCase()
+
+        allNotes.forEach((note) => {
+            const noteTitle = note.querySelector('input').value.toLowerCase()
+            note.style.display = noteTitle.includes(searchTerm) ? 'flex' : 'none'
+        })
+    }
 
     const validateForm = (existingTitle, existingContent) => {
-        const title = existingTitle ? existingTitle : noteTitle.value.trim()
-        const content = existingContent ? existingContent : noteContent.value.trim()
+        const title = existingTitle ? existingTitle : elements.noteTitle.value.trim()
+        const content = existingContent ? existingContent : elements.noteContent.value.trim()
 
-        errorMessage.textContent = ''
+        elements.errorMessage.textContent = ''
 
         if (title === '') {
-            errorMessage.textContent = 'Note title cannot be empty.'
+            elements.errorMessage.textContent = 'Note title cannot be empty.'
             return false
         }
 
         if (content === '') {
-            errorMessage.textContent = 'Note content cannot be empty.'
+            elements.errorMessage.textContent = 'Note content cannot be empty.'
             return false
         }
 
         if (title.length > 30) {
-            errorMessage.textContent = 'Note title cannot exceed 30 characters.'
+            elements.errorMessage.textContent = 'Note title cannot exceed 30 characters.'
             return false
         }
 
         if (content.length > 160) {
-            errorMessage.textContent = 'Note content cannot exceed 160 characters.'
+            elements.errorMessage.textContent = 'Note content cannot exceed 160 characters.'
             return false
         }
         return true
     }
 
-    noteTitle.addEventListener('input', () => {
-        if (!validateForm()) {
-            addButton.classList.add('hidden')
-        } else {
-            addButton.classList.remove('hidden')
-        }
-    })
-    noteContent.addEventListener('input', () => {
-        if (!validateForm()) {
-            addButton.classList.add('hidden')
-        } else {
-            addButton.classList.remove('hidden')
-        }
-    })
-
     const addNewNote = (newNote) => {
         const { title, content, date } = newNote
 
-        notesContainer.classList.remove('hidden')
+        elements.notesContainer.classList.remove('hidden')
 
         const singleNote = document.createElement('div')
         singleNote.classList.add('single-note')
 
-        notes.insertBefore(singleNote, notes.firstChild)
+        elements.notes.insertBefore(singleNote, elements.notes.firstChild)
 
         const titleInput = document.createElement('input')
         titleInput.value = title
         titleInput.disabled = true
 
-        const textareaWrapper = document.createElement('div')
-        textareaWrapper.classList.add('textarea-wrapper')
-
-        const contentInput = document.createElement('textarea')
-        contentInput.classList.add('editable-textarea')
-        contentInput.value = content
-        contentInput.disabled = true
-
-        textareaWrapper.appendChild(contentInput)
+        const contentSpan = document.createElement('span')
+        contentSpan.classList.add('editable-span')
+        contentSpan.textContent = content
+        contentSpan.contentEditable = false
 
         const dateSpan = document.createElement('span')
         dateSpan.classList.add('note-date')
@@ -112,29 +131,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const iconsWrapper = document.createElement('div')
         iconsWrapper.classList.add('icons-wrapper')
 
-        const editButton = document.createElement('button')
-        editButton.classList.add('icon-button')
-        const editIcon = document.createElement('img')
-        editIcon.src = '/icons/edit-icon.svg'
-        editButton.appendChild(editIcon)
-
-        const deleteButton = document.createElement('button')
-        deleteButton.classList.add('icon-button')
-        const deleteIcon = document.createElement('img')
-        deleteIcon.src = '/icons/bin-icon.svg'
-        deleteButton.appendChild(deleteIcon)
+        const editButton = createIconButton('/icons/edit-icon.svg')
+        const deleteButton = createIconButton('/icons/bin-icon.svg')
 
         iconsWrapper.appendChild(editButton)
         iconsWrapper.appendChild(deleteButton)
 
         singleNote.appendChild(titleInput)
-        singleNote.appendChild(textareaWrapper)
+        singleNote.appendChild(contentSpan)
         singleNote.appendChild(dateSpan)
         singleNote.appendChild(iconsWrapper)
 
         deleteButton.addEventListener('click', () => {
             showModal(() => {
-                notes.removeChild(singleNote)
+                elements.notes.removeChild(singleNote)
                 checkIfNoNotesLeft()
             })
         })
@@ -144,35 +154,42 @@ document.addEventListener('DOMContentLoaded', () => {
             saveButton.classList.add('primary-button', 'small', 'edit-button')
             saveButton.innerText = 'Save'
 
-            addAnotherNoteBtn.classList.add('hidden')
+            elements.addAnotherNoteBtn.classList.add('hidden')
             titleInput.disabled = false
-            contentInput.disabled = false
+            contentSpan.contentEditable = true
             titleInput.classList.add('default-input')
-            contentInput.classList.add('default-input')
+            contentSpan.classList.add('default-input')
 
             iconsWrapper.style.display = 'none'
-            contentInput.style.minHeight = '210px'
-            textareaWrapper.appendChild(saveButton)
+            contentSpan.focus()
+            singleNote.appendChild(saveButton)
 
             saveButton.addEventListener('click', () => {
                 const newTitle = titleInput.value.trim()
-                const newContent = contentInput.value.trim()
+                const newContent = contentSpan.textContent.trim()
                 if (validateForm(newTitle, newContent)) {
-                    addAnotherNoteBtn.classList.remove('hidden')
+                    elements.addAnotherNoteBtn.classList.remove('hidden')
                     titleInput.disabled = true
-                    contentInput.disabled = true
+                    contentSpan.contentEditable = false
                     titleInput.classList.remove('default-input')
-                    contentInput.classList.remove('default-input')
+                    contentSpan.classList.remove('default-input')
                     iconsWrapper.style.display = 'flex'
-                    contentInput.style.minHeight = ''
                     saveButton.remove()
                 } else {
-                    if (!singleNote.contains(errorMessage)) {
-                        singleNote.appendChild(errorMessage)
+                    if (!singleNote.contains(elements.errorMessage)) {
+                        singleNote.appendChild(elements.errorMessage)
                     }
                 }
             })
         })
+    }
+    const createIconButton = (iconSrc) => {
+        const button = document.createElement('button')
+        button.classList.add('icon-button')
+        const icon = document.createElement('img')
+        icon.src = iconSrc
+        button.appendChild(icon)
+        return button
     }
 
     const showModal = (onConfirm) => {
@@ -196,44 +213,17 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const checkIfNoNotesLeft = () => {
-        if (notes.children.length === 0) {
-            addAnotherNoteBtn.classList.add('hidden')
-            noNotesSection.classList.remove('hidden')
+        if (elements.notes.children.length === 0) {
+            elements.addAnotherNoteBtn.classList.add('hidden')
+            elements.noNotesSection.classList.remove('hidden')
         }
     }
 
-    noteForm.addEventListener('submit', (event) => {
-        event.preventDefault()
-
-        if (validateForm()) {
-            alert('Note added successfully!')
-            const formData = {
-                title: noteTitle.value.trim(),
-                content: noteContent.value.trim(),
-                date: new Date().toLocaleDateString('pl-PL', { month: 'long', day: 'numeric' }),
-            }
-
-            addNewNote(formData)
-
-            addButton.classList.add('hidden')
-            newNoteSection.classList.toggle('hidden')
-            addAnotherNoteBtn.classList.remove('hidden')
-            notes.style.maxHeight = '650px'
-            noteForm.reset()
-        }
-    })
-
-    searchInput.addEventListener('input', () => {
-        const allNotes = document.querySelectorAll('.single-note')
-        const searchTerm = searchInput.value.toLowerCase()
-        console.log(allNotes)
-        allNotes.forEach((note) => {
-            const noteTitle = note.querySelector('input').value.toLocaleLowerCase()
-            if (noteTitle.includes(searchTerm)) {
-                note.style.display = 'flex'
-            } else {
-                note.style.display = 'none'
-            }
-        })
-    })
+    elements.addNoteBtn.addEventListener('click', handleAddNoteClick)
+    elements.addAnotherNoteBtn.addEventListener('click', handleAddAnotherNoteClick)
+    elements.cancelBtn.addEventListener('click', handleCancelClick)
+    elements.noteTitle.addEventListener('input', handleNoteInputChange)
+    elements.noteContent.addEventListener('input', handleNoteInputChange)
+    elements.noteForm.addEventListener('submit', handleFormSubmit)
+    elements.searchInput.addEventListener('input', handleSearchInput)
 })
